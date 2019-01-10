80890c1d5e3ebcb3fdb0c270ec2cc7f239cb5cd5
clementoh <clement.oh094@gmail.com>
2019-01-10 23:06:27 +1300

    2. short-declaration operator

---
+++ 002-variables/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// short-declaration operator
+	x := 42
+	fmt.Println(x)
+	x = 49
+	fmt.Println(x)
+
+	y := 100  + 24
+	fmt.Println(y)
+
+	// var keyword
+}
