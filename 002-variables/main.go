da3e6273498802d757624acf83d09e66706fc2b8
clementoh <clement.oh094@gmail.com>
2019-01-10 23:18:32 +1300

    2. var keyword + differences

--- 002-variables/main.go
+++ 002-variables/main.go
@@ -12,5 +12,16 @@ func main() {
 	y := 100  + 24
 	fmt.Println(y)
 
-	// var keyword
+	/*
+	var
+	---
+	differences:
+		1. can be declared outside a function
+		2. can assign specific type
+		3. you can declare with identifier without assignment.
+		   A zero value of its type is given by default
+		   https://tour.golang.org/basics/12
+	*/
+	var z = 43
+	fmt.Println(z)
 }
