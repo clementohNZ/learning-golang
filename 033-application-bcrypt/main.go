99b1f8ff0043af0960a2e5e9b6c306ead2639761
clementoh <clement.oh094@gmail.com>
2019-01-14 06:55:35 +1300

    33. application - bcrypt

---
+++ 033-application-bcrypt/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+)
+
+/*
+Use: go get "golang.org/x/crypto/bcrypt"
+to download the bcrypt package
+ */
+func main() {
+	s:= `password123`
+	bs, err := bcrypt.GenerateFromPassword([]byte(s), bcrypt.DefaultCost)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(s)
+	fmt.Println(bs)
+	fmt.Println(string(bs))
+
+	loginPword := `password1234`
+	hashedPword, err := bcrypt.GenerateFromPassword([]byte(loginPword), bcrypt.DefaultCost)
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = bcrypt.CompareHashAndPassword(hashedPword, []byte(loginPword))
+	if err != nil {
+		fmt.Println("You CAN'T login!")
+		return
+	} else {
+		fmt.Println("You're logged in!")
+	}
+}
