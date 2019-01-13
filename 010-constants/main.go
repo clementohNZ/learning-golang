a0606886374050213c6a7e31501d916348ee44dc
clementoh <clement.oh094@gmail.com>
2019-01-13 16:19:13 +1300

    10. iota constant

--- 010-constants/main.go
+++ 010-constants/main.go
@@ -10,6 +10,13 @@ const (
 	a = 42
 	b float64 = 42.78
 	c string = "James Bond"
+	d = iota
+	e = iota
+	f = iota
+)
+
+const (
+	g = iota
 )
 
 func main() {
@@ -17,7 +24,18 @@ func main() {
 	fmt.Println(b)
 	fmt.Println(c)
 
-	fmt.Printf("%T\n", a)
-	fmt.Printf("%T\n", b)
-	fmt.Printf("%T\n", c)
+	// auto increments by 1 starting from
+	// the index position of the const variable
+	// d is on the 3rd line of the const block and so
+	// d is 3, not 0
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+	fmt.Printf("%T\n", d)
+	fmt.Printf("%T\n", e)
+	fmt.Printf("%T\n", f)
+
+	// resets to 0 in new const block
+	fmt.Println(g)
+	fmt.Printf("%T\n", g)
 }
