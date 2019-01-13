a39854ceb4ae8fb055ccc9313160e045b27bb237
clementoh <clement.oh094@gmail.com>
2019-01-14 06:38:08 +1300

    32. application - custom sort implementations

--- 032-application-stdlib-sort/main.go
+++ 032-application-stdlib-sort/main.go
@@ -5,6 +5,25 @@ import (
 	"sort"
 )
 
+type person struct {
+	First string
+	Age   int
+}
+
+type ByAge []person
+
+func (a ByAge) Len() int           { return len(a) }
+func (a ByAge) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByAge) Less(i, j int) bool { return a[i].Age < a[j].Age }
+
+
+type ByName []person
+
+func (bn ByName) Len() int           { return len(bn) }
+func (bn ByName) Swap(i, j int)      { bn[i], bn[j] = bn[j], bn[i] }
+func (bn ByName) Less(i, j int) bool { return bn[i].First < bn[j].First }
+
+
 func main() {
 	xi := []int{4, 7, 3, 42, 99, 18, 16, 56, 12}
 	xs := []string{"James", "Q", "M", "Moneypenny", "Dr. No"}
@@ -18,4 +37,27 @@ func main() {
 
 	sort.Strings(xs)
 	fmt.Println(xs)
+
+	// Custom sorting
+	fmt.Println("-------")
+	p1 := person{"James", 32}
+	p2 := person{"Moneypenny", 27}
+	p3 := person{"Q", 64}
+	p4 := person{"M", 56}
+
+	people := []person{p1, p2, p3, p4}
+
+	fmt.Println(people)
+
+	// ByAge implements sort.Interface for []Person based on
+	// the Age field.
+	fmt.Println(people)
+	// There are two ways to sort a slice. First, one can define
+	// a set of methods for the slice type, as with ByAge, and
+	// call sort.Sort. In this first example we use that technique.
+	sort.Sort(ByAge(people))
+	fmt.Println(people)
+
+	sort.Sort(ByName(people))
+	fmt.Println(people)
 }
