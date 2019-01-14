9ec5087d7ee819a4887b4f980301ff48a08c7cfd
clementoh <clement.oh094@gmail.com>
2019-01-14 13:26:02 +1300

    34. concurrency v. parallelism

---
+++ 034-concurrency-concurrency-v-parallelism/main.go
@@ -0,0 +1,16 @@
+package main
+
+/*
+Go was built after dual-core processors became widely available
+in 2006.
+
+https://blog.golang.org/concurrency-is-not-parallelism
+
+Concurrency
+- Design pattern
+- Write code that has the potential to run in parallel if you have multiple cores
+
+Parallel
+- You need more than once CPU so code can execute simultaneously in separate cpus
+ */
+func main() {}
